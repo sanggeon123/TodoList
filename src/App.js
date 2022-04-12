8077a129f8ad85bb9be5c4d5d2787426a4932dc8
sanggeon123 <92092122+sanggeon123@users.noreply.github.com>
2022-04-12 12:52:25 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {createGlobalStyle} from 'styled-components';
+import TodoTemplate from './components/todoTemplate';
+import TodoHeader from './components/todoHeader';
+import TodoList from './components/todoList';
+import TodoCreate from './components/todoCreate';
+import {TodoProvider} from './todoContext';
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background-color: #FFDDDD;
+  }
+`;
+
+function App(){
+  return (
+    <>
+      <TodoProvider>
+          <GlobalStyle/>
+          <TodoTemplate>
+            <TodoHeader/>
+            <TodoList/>
+            <TodoCreate/>
+          </TodoTemplate>
+      </TodoProvider>
+    </>
+  );
+}
+
+export default App;
